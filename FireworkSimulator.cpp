1b327673a83a758b535c5a3a7c9442580a193392
Senney <sean.heintz@gmail.com>
2011-12-03 02:54:59 -0700

    Added some objects. Working on window system.

--- FireworkSimulator.cpp
+++ FireworkSimulator.cpp
@@ -6,11 +6,12 @@
 
 // Core includes
 #include "Math/Point2D.h"
+#include "Physics/PhysicsHandler.h"
 
 int main(int argv, char** argc) { 
 	std::cout << "Fireworks Simulator : Under Construction!" << std::endl;
 	
-	
+	Physics::PhysicsHandler physicsHandler();		
 	
 	return 0;	
 }
